9ea52a15ab150dba5c51d992015e2e6021dc5923
daniel-kav <dankavatha01@gmail.com>
2024-12-17 10:35:04 +0300

    day

--- try.php
+++ try.php
@@ -8,12 +8,12 @@
 
 // // Find your Account SID and Auth Token at twilio.com/console
 // // and set the environment variables. See http://twil.io/secure
-// $sid = "AC132d4e0fec806ff3d83895397913f314";
-// $token = "8eb4ea6551346a783f319babd2275c56";
+// $sid = "SID";
+// $token = "TOKEN";
 // $twilio = new Client($sid, $token);
 
 // $verification = $twilio->verify->v2
-//     ->services("VA5e092e676301c2185ea94deba9dd8d41")
+//     ->services("VA")
 //     ->verifications->create(
 //         "+254768441549", // To
 //         "sms" // Channel
@@ -27,15 +27,16 @@
 require_once 'vendor/autoload.php';
 use Twilio\Rest\Client;
 
-$sid    = "AC132d4e0fec806ff3d83895397913f314";
-$token  = "8eb4ea6551346a783f319babd2275c56";
+$sid    = "sid";
+$token  = "token";
 $twilio = new Client($sid, $token);
 
-$verification_check = $twilio->verify->v2->services("VA5e092e676301c2185ea94deba9dd8d41")
+$verification_check = $twilio->verify->v2->services("VAA")
                                    ->verificationChecks
                                    ->create([
                                                 "to" => "+254768441549",
-                                                "code" => "[Code]"
+                                                "code" => "35672"
+                                                
                                             ]
                                     );
 
